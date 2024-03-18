a9c9500e5cb30182107b055000f0c2ded7b2ad29
a-dugonin <a_dugonin@mail.ru>
2024-03-18 20:29:00 +0300

    task_1: Добавление описания классам и методам и добавление аннотаций типов

--- task_1/context_manager.py
+++ task_1/context_manager.py
@@ -1,15 +1,26 @@
+from typing import Iterator
+
 from loggers import get_logger
 
 logger = get_logger(__name__)
 
 
 class File:
-    def __init__(self, filename, mode):
+    """
+    Класс для реализации контекст-менеджера File
+    Аттрибуты:
+        filename - имя файла
+        mode - режим для открытия файла
+        file_obj - файловый объект, по умолчанию имеет значение None
+    """
+    def __init__(self, filename: str, mode: str) -> None:
         self.filename = filename
         self.mode = mode
         self.file_obj = None
 
-    def __enter__(self):
+    def __enter__(self) -> Iterator:
+        """ Метод для создания файлового объекта и открытия файла. Если указанного файла не существует, то будет создан
+         файл в текущей директории с указанным именем с режимом для записи """
         try:
             self.file_obj = open(self.filename, self.mode)
             logger.info(f"Файл открыт: {self.filename}, режим - {self.mode}")
@@ -21,7 +32,9 @@ class File:
             )
         return self.file_obj
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: str, exc_val: str, exc_tb: str) -> bool:
+        """ Метод для закрытия файла по завершению работы с ним. Любые ошибки которые могут возникнуть при закрытии
+        файла игнорируются что позволяет закрыть файл при любых условиях """
         self.file_obj.close()
         logger.info(f"Файл {self.filename} закрыт")
         return True
