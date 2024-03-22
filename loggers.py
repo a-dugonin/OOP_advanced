4deaf264e6c179ba3fcde374fb912f8d624d4eab
a-dugonin <a_dugonin@mail.ru>
2024-03-22 18:52:43 +0300

    task_3: В рамках задачи реализован класс Date позволяющий создавать объекты класса на основе строкового значения даты

--- loggers.py
+++ loggers.py
@@ -4,7 +4,7 @@ import logging.config
 
 
 def get_logger(name, template='default'):
-    with open('loggers.json', "r") as json_file:
+    with open('/home/anton/PycharmProjects/OOP_advanced/loggers.json', "r") as json_file:
         dict_config = json.load(json_file)
         dict_config["loggers"][name] = dict_config["loggers"][template]
     logging.config.dictConfig(dict_config)
